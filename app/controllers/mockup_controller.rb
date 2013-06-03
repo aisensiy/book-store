d72d3a31fa95fb65a0b3ede20493ea35ae2941dd
aisensiy <aisensiy@gmail.com>
2013-06-03 20:47:26 +0800

    init with mockup

---
+++ app/controllers/mockup_controller.rb
@@ -0,0 +1,22 @@
+class MockupController < ApplicationController
+  def books_list
+  end
+
+  def sign_in
+  end
+
+  def sign_up
+  end
+
+  def password_reset
+  end
+
+  def password_modify
+  end
+
+  def book
+  end
+
+  def upload_book
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BooksLists::Application.config.secret_token = '410813056225e38880862dc2f62d7988a6fc2ef6670ddf87ba69771925efdbe38dd4e46492ef6e853d87fb59f2c9f7ff4c88c0c278f6908fd41228426c7d64f0'

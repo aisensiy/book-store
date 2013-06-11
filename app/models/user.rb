ad135fd710e8eba4cceec627c8d60bb1356d3920
aisensiy <aisensiy@gmail.com>
2013-06-11 21:53:49 +0800

    add delete user and make test pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < BaseClient
   def self.signup(options)
     post('/1/users', body: JSON.dump(options))
   end
+
+  def self.remove(user_id, token)
+    headers = self.headers.merge({'X-Parse-Session-Token' => token})
+    delete("/1/users/#{user_id}", headers: headers)
+  end
 end
--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,10 +1,21 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  setup do
+    @users_id = []
+  end
+
   test "should create a new user" do
     post :create, user: {username: 'thisisatest', password: '000000a', email: 'aisensiy@gmail.com'}
     result = JSON.parse(@response.body)
     assert_equal result['objectId'], session[:user_id]
     assert_equal result['sessionToken'], session[:token]
+    @users_id << session[:user_id]
+  end
+
+  teardown do
+    @users_id.each do |user_id|
+      User.remove(user_id, session[:token])
+    end
   end
 end

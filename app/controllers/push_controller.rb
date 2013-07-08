06c0ecdb6f79f7c7ec7da5509aa01bcb5c30b563
aisensiy <aisensiy@gmail.com>
2013-07-08 12:12:37 +0800

    bug fix for push

--- app/controllers/push_controller.rb
+++ app/controllers/push_controller.rb
@@ -1,6 +1,6 @@
 class PushController < ApplicationController
   def push
-    resp = Push.push_to_device(params[:data])
+    resp = Push.push_to_device(params)
     render status: resp.code, json: resp.body
   end
 end
--- app/models/push.rb
+++ app/models/push.rb
@@ -1,5 +1,5 @@
 class Push < BaseClient
    def self.push_to_device(data)
-     post('/1/push', body: data)
+     post('/1/push', body: JSON.dump(data))
    end
 end

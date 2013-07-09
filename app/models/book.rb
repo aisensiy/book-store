be38294ea8722534de1f21aaf8da391f5fdfac28
aisensiy <aisensiy@gmail.com>
2013-07-09 15:33:57 +0800

    add header

--- app/models/book.rb
+++ app/models/book.rb
@@ -19,6 +19,7 @@ class Book < BaseClient
   end
 
   def self.get_books(limit=40, skip=0)
-    get("/1/classes/Book", query: {count: 1, limit: limit, skip: skip, keys: 'title,author,cover_url'})
+    get("/1/classes/Book", query: {count: 1, limit: limit, skip: skip, keys: 'title,author,cover_url', order: '-createdAt'})
   end
+
 end

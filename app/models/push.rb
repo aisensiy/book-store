b3ff0fb4f99d7a3a7bfae8bdf09a886001adf460
aisensiy <aisensiy@gmail.com>
2013-08-03 16:57:02 +0800

    add DownloadFile and push the new objectId to device

--- app/models/push.rb
+++ app/models/push.rb
@@ -4,7 +4,19 @@ class Push < BaseClient
    end
 
    def self.send_url_to_user_device(email, type, item_id)
-     data = {'where' => {'owner' => {'$inQuery' => {'email' => email}}}, 'data' => {'alert' => "#{type} #{item_id}"}}
+     obj = Parse.get type.classify, item_id
+     url = type.classify.constantize.get_download_token obj['file_key'], obj['file_name']
+     download_file_id = self.create_download_file type, url
+     data = {'where' => {'owner' => {'$inQuery' => {'email' => email}}}, 'data' => {'alert' => download_file_id}}
      self.push_to_device(data)
+   end
+
+   private
+   def self.create_download_file(type, url)
+     download_file = Parse::Object.new('DownloadFile')
+     download_file['type'] = type
+     download_file['url'] = url
+     result = download_file.save
+     result['objectId']
    end
 end
---
+++ config/initializers/parse_client.rb
@@ -0,0 +1,4 @@
+require 'parse-ruby-client'
+
+Parse.init application_id: Settings.parse_appid,
+           api_key: Settings.parse_restid

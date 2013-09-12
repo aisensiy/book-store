d66057f5a38650b2081131019e0cb31c48122079
aisensiy <aisensiy@gmail.com>
2013-09-12 21:35:15 +0800

    add link to change language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # reset captcha code after each request for security
   after_filter :reset_last_captcha_code!
+  before_filter :set_locale
 
   protected
   def signin?
@@ -8,5 +9,13 @@ class ApplicationController < ActionController::Base
       render status: 403, json: {error: 'not signin'}
       return false
     end
+  end
+
+  def set_locale
+    if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+      session[:locale] = params[:locale]
+    end
+
+    I18n.locale = session[:locale] || I18n.default_locale
   end
 end

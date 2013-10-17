cca145bc92f57e4dabca8b6cffb2308dd37fe65e
aisensiy <aisensiy@gmail.com>
2013-10-18 01:50:40 +0800

    add mina deploy with god unicorn nginx

---
+++ lib/mina/servers/production.rb
@@ -0,0 +1,16 @@
+# FreeBSD
+namespace :env do
+  task :production => [:environment] do
+    set :domain,              '192.168.33.10'
+    set :deploy_to,           '/home/deployer/apps/bookstore'
+    set :sudoer,              'sudoer_user'
+    set :user,                'deployer'
+    set :group,               'admin'
+    # set :rvm_path,          '/usr/local/rvm/scripts/rvm'   # we don't use that. see below.
+    set :services_path,       '/etc/init.d'          # where your God and Unicorn service control scripts will go
+    set :nginx_path,          '/etc/nginx'
+    set :deploy_server,       'bookstore'                   # just a handy name of the server
+    invoke :defaults                                         # load rest of the config
+    # invoke :"rvm:use[#{rvm_string}]"                       # since my prod server runs 1.9 as default system ruby, there's no need to run rvm:use
+  end
+end

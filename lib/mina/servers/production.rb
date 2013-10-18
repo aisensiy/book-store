d68fd4b6ca43784049650d409aecb0a1b1d0bf1b
aisensiy <aisensiy@gmail.com>
2013-10-18 13:43:59 +0800

    mv config files

--- lib/mina/servers/production.rb
+++ lib/mina/servers/production.rb
@@ -7,9 +7,10 @@ namespace :env do
     set :user,                'deployer'
     set :group,               'admin'
     # set :rvm_path,          '/usr/local/rvm/scripts/rvm'   # we don't use that. see below.
-    set :services_path,       '/etc/init.d'          # where your God and Unicorn service control scripts will go
+    set :services_path,       '/etc/init.d'                  # where your God and Unicorn service control scripts will go
     set :nginx_path,          '/etc/nginx'
-    set :deploy_server,       'bookstore'                   # just a handy name of the server
+    set :deploy_server,       'bookstore'                    # just a handy name of the server
+    set :unicorn_workers,     2
     invoke :defaults                                         # load rest of the config
     # invoke :"rvm:use[#{rvm_string}]"                       # since my prod server runs 1.9 as default system ruby, there's no need to run rvm:use
   end
